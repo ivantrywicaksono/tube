c5a52ed64c6a63f29c402c9e877a9a148ee2043e
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2023-08-15 14:11:28 +0700

    initial develop branch change logo images and create required folders in src

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import logo from './assets/logo512.png';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <>
+      <img src={logo}></img>
     </>
   );
 }
