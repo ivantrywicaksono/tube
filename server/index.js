1095e287f36c532ae00b025a5b40acf7585d6911
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2023-08-23 08:12:10 +0700

    feat: add db connection file config and axios in client directory

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
+const pool = require('./db');
 dotenv.config();
 
 const app = express();
@@ -12,10 +12,13 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Homepage');
+app.get('/', async (req, res) => {
+  res.send({ page: 'homepage' });
 });
 
+app.post('/transaction', async (req, res) => {
+  const { date, name, category, nominal, quantity } = req.body;
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on the port ${PORT}`);
